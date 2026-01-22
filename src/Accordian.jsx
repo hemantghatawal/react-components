3a1491140a841d0dabd4c115160ca766d0ab5e48
Hemant Ghatawal <ghatawal35@gmail.com>
2026-01-22 13:23:00 +0530

    accordian refactored and improved

--- src/Accordian.jsx
+++ src/Accordian.jsx
@@ -1,40 +1,57 @@
 import { useState } from "react";
 
-const accordianDefault = { a: 0, b: 0, c: 0 };
+const accordianDefault = { a: false, b: false, c: false };
 export default function Accordion() {
   const [accordianState, setAccordianState] = useState(accordianDefault);
   console.log("accordianState =>", accordianState);
   return (
     <div>
       <div>
-        <div onClick={() => setAccordianState((s) => ({ ...s, a: !s.a }))}>
+        <div
+          onClick={() => setAccordianState((s) => ({ ...s, a: !s.a }))}
+          aria-expanded={accordianState.a}
+        >
           {" "}
           HTML{" "}
           <span
             aria-hidden={true}
-            className={`accordion-icon ${!accordianState.a ? "accordion-icon--rotated" : ""} `}
+            className={`accordion-icon ${!accordianState.a ? "accordion-icon--rotated" : ""}`}
           />
         </div>
-        <div className={` ${accordianState.a ? "show" : "hide"} `}>
+        <div className={` ${accordianState.a ? "show" : "hide"}`}>
           The HyperText Markup Language or HTML is the standard markup language
           for documents designed to be displayed in a web browser.
         </div>
       </div>
       <div>
-        <div onClick={() => setAccordianState((s) => ({ ...s, b: !s.b }))}>
-          CSS <span aria-hidden={true} className={`accordion-icon ${!accordianState.a ? "accordion-icon--rotated" : ""} `} />
+        <div
+          onClick={() => setAccordianState((s) => ({ ...s, b: !s.b }))}
+          aria-expanded={accordianState.b}
+        >
+          CSS{" "}
+          <span
+            aria-hidden={true}
+            className={`accordion-icon ${!accordianState.b ? "accordion-icon--rotated" : ""}`}
+          />
         </div>
-        <div>
+        <div className={` ${accordianState.b ? "show" : "hide"}`}>
           Cascading Style Sheets is a style sheet language used for describing
           the presentation of a document written in a markup language such as
           HTML or XML.
         </div>
       </div>
       <div>
-        <div onClick={() => setAccordianState((s) => ({ ...s, c: !s.c }))}>
-          JavaScript <span aria-hidden={true} className={`accordion-icon ${!accordianState.a ? "accordion-icon--rotated" : ""} `} />
+        <div
+          onClick={() => setAccordianState((s) => ({ ...s, c: !s.c }))}
+          aria-expanded={accordianState.c}
+        >
+          JavaScript{" "}
+          <span
+            aria-hidden={true}
+            className={`accordion-icon ${!accordianState.c ? "accordion-icon--rotated" : ""}`}
+          />
         </div>
-        <div>
+        <div className={` ${accordianState.c ? "show" : "hide"}`}>
           JavaScript, often abbreviated as JS, is a programming language that is
           one of the core technologies of the World Wide Web, alongside HTML and
           CSS.
